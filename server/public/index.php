b7f54636e729ffff394aed0de02330924b6b3006
Morteza Sohouli <morteza.sohouli@gmail.com>
2026-02-16 00:57:40 +0330

    added tauri origin

--- server/public/index.php
+++ server/public/index.php
@@ -49,6 +49,7 @@ $app->add(function (Request $request, RequestHandler $handler)
         'http://localhost:3000',
         'https://localhost',
         'http://localhost:5173',
+        'tauri://localhost'
     ]);
     $origin = $request->getHeaderLine('Origin');
     $allowOrigin = in_array($origin, $allowedOrigins, true) ? $origin : null;
